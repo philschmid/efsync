575b975792e95759008a5bcb928e0f8eeb839230
Philipp Schmid <philippschmid@Philipps-MBP-2.fritz.box>
2020-08-17 21:34:29 +0200

    fixed permission

--- efsync/utils/ssh_key.py
+++ efsync/utils/ssh_key.py
@@ -7,7 +7,7 @@ def write_key_to_file(file_string=None, file_path='.efsync', key_name=''):
     try:
         with open(f'{file_path}/{key_name}.pem', 'w') as out_file:
             out_file.write(file_string)
-        os.chmod(f'{file_path}/{key_name}.pem' , 0400)
+        os.chmod(f'{file_path}/{key_name}.pem' , 0o400)
     except Exception as e:
         print(e)
         raise(e)
