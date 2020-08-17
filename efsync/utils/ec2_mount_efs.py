058f046bf226d3d975defcfe4d119c3efb90db98
Philipp Schmid <philippschmid@Philipps-MBP-2.fritz.box>
2020-08-17 22:15:22 +0200

    added logs to ssh

--- efsync/utils/ec2_mount_efs.py
+++ efsync/utils/ec2_mount_efs.py
@@ -1,23 +1,25 @@
 import boto3
 import paramiko
 from efsync.utils.helper.createSSHClient import createSSHClient
+from efsync.logger import get_logger
+logger = get_logger()
 
 
-
-def mount_efs(bt3=None, instance_id=None, efs_filesystem_id=None, clean_efs=None,ec2_key_name=None):
+def mount_efs(bt3=None, instance_id=None, efs_filesystem_id=None, clean_efs=None, ec2_key_name=None):
     try:
         client = bt3.client('ec2')
         response = client.describe_instances(InstanceIds=[instance_id])
         # get public dns
         public_dns_name = response['Reservations'][0]['Instances'][0]['NetworkInterfaces'][0]['Association']['PublicDnsName']
         # connect ssh
-        ssh = createSSHClient(public_dns_name=public_dns_name,ec2_key_name= ec2_key_name)
+        ssh = createSSHClient(
+            public_dns_name=public_dns_name, ec2_key_name=ec2_key_name)
         # install efs mount helper
         stdin, stdout, stderr = ssh.exec_command(
             'sudo yum install -y amazon-efs-utils')
         stdin.flush()
-        data = stdout.read().splitlines()
-        # @TODO: Add logger
+        # data =
+        logger.info(stdout.read().decode('utf-8'))
         # mount efs to ec2
         stdin, stdout, stderr = ssh.exec_command(
             'sudo mkdir efs')
@@ -26,20 +28,18 @@ def mount_efs(bt3=None, instance_id=None, efs_filesystem_id=None, clean_efs=None
         stdin, stdout, stderr = ssh.exec_command(
             f' mount | grep /home/ec2-user/efs')
         stdin.flush()
-        data = stdout.read().splitlines()
-        # @TODO: Add logger
+        logger.info(stdout.read().decode('utf-8'))
         # chown for scp
         stdin, stdout, stderr = ssh.exec_command(
             'sudo chown -R ec2-user:ec2-user efs')
         stdin.flush()
-        data = stdout.read().splitlines()
-        # @TODO: Add logger
+        logger.info(stdout.read().decode('utf-8'))
         # clean efs if wanted
         if(clean_efs):
             stdin, stdout, stderr = ssh.exec_command(
                 ' rm -r efs/*')
             stdin.flush()
-            data = stdout.read().splitlines()
+            logger.info(stdout.read().decode('utf-8'))
 
     except Exception as e:
         print(e)
