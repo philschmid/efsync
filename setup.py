45e9c865f7b7d52ad43e26eced4167c213e1009b
Philipp Schmid <philippschmid@Philipps-MBP-2.fritz.box>
2020-08-17 21:34:44 +0200

    version pump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ print(find_packages())
 
 setup(
     name='efsync',
-    version='0.2.3',
+    version='0.2.4',
     packages=find_packages(),
     entry_points={
         "console_scripts": ["efsync=efsync.efsync_cli:main"]
